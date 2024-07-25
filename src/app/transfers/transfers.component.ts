5fa9d8b762887bcb24396523f8f1bb2778de1577
Franco-Spinelli <spinellifranco04@gmail.com>
2024-07-25 18:50:19 -0300

    add loading circle in transfers-component

--- src/app/transfers/transfers.component.ts
+++ src/app/transfers/transfers.component.ts
@@ -4,6 +4,7 @@ import { UserServiceService } from '../services/user-service.service';
 
 import { CreateTransfer } from '../services/createTransfer';
 import { Router } from '@angular/router';
+import { LoadingCircleComponent } from '../loading-circle/loading-circle.component';
 
 @Component({
   selector: 'app-transfers',
@@ -12,13 +13,16 @@ import { Router } from '@angular/router';
 })
 export class TransfersComponent {
   transferForm: FormGroup;
-  constructor(private fb: FormBuilder,private userServie: UserServiceService, private router: Router){
+  isLoading = false;
+  constructor(private fb: FormBuilder,private userServie: UserServiceService, private router: Router, private loadingCircle: LoadingCircleComponent){
     this.transferForm = this.fb.group({
       recipient: ['', [Validators.required, Validators.minLength(4)]],
       transferAmount: [0, [Validators.required, Validators.min(100)]]
     });
   }
   onSubmit() {
+    this.isLoading = true;
+    this.loadingCircle.startLoading();
     if (this.transferForm.valid) {
       const formValue = this.transferForm.value;
       const transferDTO: CreateTransfer = {
@@ -34,10 +38,14 @@ export class TransfersComponent {
 
       this.userServie.postTransfer(transferDTO).subscribe((data)=>{
         alert("Transfer success!");
+        this.isLoading = false;
+        this.loadingCircle.stopLoading();
         this.router.navigateByUrl("/user-dashboard")
       
       }  ,(error)=>{
           alert("error unexpected! Please check the recipient data or your balance!")
+          this.isLoading = false;
+          this.loadingCircle.stopLoading();
       })
       
     } else {
